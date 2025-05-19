1b60aa6df8fdf9f0c7a9cc9c0df85d09e0f9f3ed
Maria Ghandour <mariaghandour@Marias-MacBook-Air.local>
2025-05-19 14:21:33 +0200

    docs: fix punctuation and consistency

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,4 @@
-//! # ProofError: Error Types for Zero-Knowledge Proofs
+//! # ProofError: Error Types for Zero-Knowledge Proofs.
 //!
 //! This module defines the [`ProofError`] enum, which encapsulates possible errors that may occur
 //! during the execution of Sigma protocols or their non-interactive variants.
@@ -6,8 +6,8 @@
 //! These errors include:
 //! - Verification failures (e.g., when a proof does not verify correctly).
 //! - Mismatched parameters during batch verification.
-//! - Not implemented methods
-//! - Group element/scalar serialization failed
+//! - Not implemented methods.
+//! - Group element/scalar serialization failed.
 use thiserror::Error;
 /// An error during proving or verification, such as a verification failure.
 #[derive(Debug, Error)]
@@ -19,9 +19,9 @@ pub enum ProofError {
     #[error("Mismatched parameter sizes for batch verification.")]
     BatchSizeMismatch,
     /// Occurs when a feature is not implemented yet.
-    #[error("The method is not yet implemented for this struct")]
+    #[error("The method is not yet implemented for this struct.")]
     NotImplemented(&'static str),
     /// Serialization of a group element/scalar failed
-    #[error("Serialization of a group element/scalar failed")]
+    #[error("Serialization of a group element/scalar failed.")]
     GroupSerializationFailure,
 }
