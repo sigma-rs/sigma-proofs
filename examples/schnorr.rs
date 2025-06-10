83c8461642875f9dff59c34223bc3acf650b6836
GOURIOU Lénaïck <lg@leanear.io>
2025-06-10 17:26:12 +0200

    feat(examples): simplified dlog example using the discrete log function defined in tests

--- examples/schnorr.rs
+++ examples/schnorr.rs
@@ -3,8 +3,8 @@
 //! Proves knowledge of `x` such that `P = x * G`, using the high-level LinearRelation API.
 
 use curve25519_dalek::RistrettoPoint;
-use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
 use curve25519_dalek::scalar::Scalar;
+use group::{Group, GroupEncoding};
 use rand::rngs::OsRng;
 
 use sigma_rs::codec::ShakeCodec;
@@ -12,27 +12,35 @@ use sigma_rs::fiat_shamir::NISigmaProtocol;
 use sigma_rs::linear_relation::LinearRelation;
 use sigma_rs::schnorr_protocol::SchnorrProtocol;
 
+// We should probably have global access to these useful morphism constructing primitives instead of putting them behind tests?
+#[allow(non_snake_case)]
+pub fn discrete_logarithm<G: Group + GroupEncoding>(
+    x: G::Scalar,
+) -> (LinearRelation<G>, Vec<G::Scalar>) {
+    let mut morphismp: LinearRelation<G> = LinearRelation::new();
+
+    let var_x = morphismp.allocate_scalar();
+    let var_G = morphismp.allocate_element();
+
+    let var_X = morphismp.allocate_eq(var_x * var_G);
+
+    morphismp.assign_element(var_G, G::generator());
+    morphismp.compute_image(&[x]).unwrap();
+
+    let X = morphismp.morphism.group_elements.get(var_X).unwrap();
+
+    assert_eq!(X, G::generator() * x);
+    (morphismp, vec![x])
+}
+
 #[allow(non_snake_case)]
 fn main() {
     // === Setup: define witness and statement ===
     let x = Scalar::random(&mut OsRng); // Prover's secret
-    let G = RISTRETTO_BASEPOINT_POINT; // Public generator
-    let P = x * G; // Public statement: P = x * G
-
-    // === Build the relation P = x * G ===
-    let mut relation = LinearRelation::new();
-
-    let svar = relation.allocate_scalar(); // Allocate variable for x
-    let gvar = relation.allocate_element(); // Variable for G
-    let pvar = relation.allocate_element(); // Variable for P
-
-    relation.assign_element(gvar, G); // Assign known base
-    relation.assign_element(pvar, P); // Assign public statement
-
-    relation.constrain(pvar, [(svar, gvar)]); // Encode P = x * G
+    let (relation, _) = discrete_logarithm(x);
 
     // === Create Schnorr protocol and wrap with Fiat-Shamir ===
-    let schnorr = SchnorrProtocol::from(relation);
+    let schnorr = SchnorrProtocol::<RistrettoPoint>::from(relation);
     let ni = NISigmaProtocol::<_, ShakeCodec<RistrettoPoint>>::new(b"schnorr-example", schnorr);
 
     // === Prove ===
