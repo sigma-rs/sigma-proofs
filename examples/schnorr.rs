5412ec26a07d6675f2657d7925ddaa4e7b95c55a
Michele Orrù <m@orru.net>
2025-09-12 23:35:32 +0200

    refactor: update package metadata `sigma-rs` -> `sigma-proofs` (#86)

--- examples/schnorr.rs
+++ examples/schnorr.rs
@@ -34,14 +34,14 @@ fn create_relation(P: RistrettoPoint) -> LinearRelation<RistrettoPoint> {
 /// generate a proof that P = x * G
 #[allow(non_snake_case)]
 fn prove(x: Scalar, P: RistrettoPoint) -> ProofResult<Vec<u8>> {
-    let nizk = create_relation(P).into_nizk(b"sigma-rs::examples");
+    let nizk = create_relation(P).into_nizk(b"sigma-proofs-example");
     nizk?.prove_batchable(&vec![x], &mut OsRng)
 }
 
 /// Verify a proof of knowledge of discrete logarithm for the given public key P
 #[allow(non_snake_case)]
 fn verify(P: RistrettoPoint, proof: &[u8]) -> ProofResult<()> {
-    let nizk = create_relation(P).into_nizk(b"sigma-rs::examples");
+    let nizk = create_relation(P).into_nizk(b"sigma-proofs-example");
     nizk?.verify_batchable(proof)
 }
 
