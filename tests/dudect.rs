f253f732129a6520bfd83228a1cf6938153643f9
Victor Graf <victor@subgraf.dev>
2026-01-28 21:34:32 -0800

    make dudect tests run in serial, and reduce the number of default iterations.

--- tests/dudect.rs
+++ tests/dudect.rs
@@ -2,9 +2,8 @@
 mod relations;
 
 use std::{
-    cmp,
-    fmt::Display,
     hint::black_box,
+    sync::LazyLock,
     time::{Duration, Instant},
 };
 
@@ -16,6 +15,7 @@ use group::Group;
 use rand::seq::SliceRandom;
 use rand_chacha::{ChaCha12Rng, rand_core::SeedableRng};
 use relations::TestRng;
+use serial_test::serial;
 use sigma_proofs::{
     LinearRelation, Nizk,
     codec::Shake128DuplexSponge,
@@ -24,10 +24,18 @@ use sigma_proofs::{
     traits::{SigmaProtocol, SigmaProtocolSimulator},
 };
 
+use crate::stats::{CtSummary, ct_stats};
+
 /// Maximum value for the max_t value. If the T value is higher, the test will fail.
 const T_VALUE_THRESHOLD: f64 = 20.0;
 /// Number of samples to take when comparing two distributions.
-const SAMPLES: usize = 10000;
+static SAMPLES: LazyLock<usize> = LazyLock::new(|| match std::env::var("DUDECT_SAMPLES") {
+    Ok(string) => string
+        .parse::<usize>()
+        .expect("DUDECT_SAMPLES env var is not a valid count"),
+    Err(std::env::VarError::NotPresent) => 100,
+    Err(std::env::VarError::NotUnicode(_)) => panic!("DUDECT_SAMPLES env var is not unicode"),
+});
 
 mod relation_ct_tests {
     use super::*;
@@ -35,6 +43,7 @@ mod relation_ct_tests {
     macro_rules! relation_ct_test {
         ($name:ident) => {
             #[test]
+            #[serial]
             fn $name() {
                 set_core_affinity().ok();
                 let stats = compare::<CanonicalLinearRelation<G>>(
@@ -64,8 +73,10 @@ mod relation_ct_tests {
     relation_ct_test!(elgamal_subtraction);
 }
 
+/// Test to establish a baseline noise on a given system, other tests are likely to fail with false
+/// positives as well.
 #[test]
-#[ignore = "used to establish a baseline noise on a given system"]
+#[serial]
 fn baseline() {
     set_core_affinity().ok();
     let stats = compare::<CanonicalLinearRelation<G>>(
@@ -91,6 +102,7 @@ fn wide_relation<const WIDTH: usize>(
 }
 
 #[test]
+#[serial]
 fn test_composition_left_right() {
     set_core_affinity().ok();
     let stats = compare(
@@ -107,7 +119,7 @@ fn compare<P: SigmaProtocol<Challenge = Scalar> + SigmaProtocolSimulator>(
     mut left: impl InstanceDist<Protocol = P>,
     mut right: impl InstanceDist<Protocol = P>,
 ) -> CtSummary {
-    let (left_times, right_times): (Vec<u64>, Vec<u64>) = (0..SAMPLES)
+    let (left_times, right_times): (Vec<u64>, Vec<u64>) = (0..*SAMPLES)
         .map(|_| {
             (
                 time_prove(left()).as_nanos() as u64,
@@ -260,188 +272,192 @@ where
     type Protocol = P;
 }
 
-// The following code is copied from the dudect_bencher, then modified here.
-//
-// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+mod stats {
+    // The following code is copied from the dudect_bencher, then modified here.
+    //
+    // Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+    // file at the top-level directory of this distribution and at
+    // http://rust-lang.org/COPYRIGHT.
+    //
+    // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+    // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+    // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+    // option. This file may not be copied, modified, or distributed
+    // except according to those terms.
 
-#[derive(Copy, Clone, Debug, Default, PartialEq)]
-pub struct CtSummary {
-    pub max_t: f64,
-    pub max_tau: f64,
-    pub sample_size: usize,
-}
+    use std::{cmp, fmt::Display};
 
-impl Display for CtSummary {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let &CtSummary {
+    #[derive(Copy, Clone, Debug, Default, PartialEq)]
+    pub struct CtSummary {
+        pub max_t: f64,
+        pub max_tau: f64,
+        pub sample_size: usize,
+    }
+
+    impl Display for CtSummary {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let &CtSummary {
+                max_t,
+                max_tau,
+                sample_size,
+            } = self;
+            write!(
+                f,
+                "n == {:+0.3}M, max t = {:+0.5}, max tau = {:+0.5}, (5/tau)^2 = {}",
+                (sample_size as f64) / 1_000_000f64,
+                max_t,
+                max_tau,
+                (5f64 / max_tau).powi(2) as usize
+            )
+        }
+    }
+
+    #[derive(Copy, Clone, Debug, Default)]
+    struct CtTest {
+        means: (f64, f64),
+        sq_diffs: (f64, f64),
+        sizes: (usize, usize),
+    }
+
+    // NaNs are smaller than everything
+    fn local_cmp(x: f64, y: f64) -> cmp::Ordering {
+        use std::cmp::Ordering::{Equal, Greater, Less};
+        if y.is_nan() {
+            Greater
+        } else if x.is_nan() || x < y {
+            Less
+        } else if x == y {
+            Equal
+        } else {
+            Greater
+        }
+    }
+
+    /// Helper function: extract a value representing the `pct` percentile of a sorted sample-set,
+    /// using linear interpolation. If samples are not sorted, return nonsensical value.
+    fn percentile_of_sorted(sorted_samples: &[f64], pct: f64) -> f64 {
+        assert!(!sorted_samples.is_empty());
+        if sorted_samples.len() == 1 {
+            return sorted_samples[0];
+        }
+        let zero = 0f64;
+        assert!(zero <= pct);
+        let hundred = 100f64;
+        assert!(pct <= hundred);
+        let length = (sorted_samples.len() - 1) as f64;
+        let rank = (pct / hundred) * length;
+        let lrank = rank.floor();
+        let d = rank - lrank;
+        let n = lrank as usize;
+        let lo = sorted_samples[n];
+        let hi = sorted_samples[n + 1];
+        lo + (hi - lo) * d
+    }
+
+    /// Return the percentiles at f(1), f(2), ..., f(100) of the runtime distribution, where
+    /// `f(k) = 1 - 0.5^(10k / 100)`
+    pub fn prepare_percentiles(durations: &[u64]) -> Vec<f64> {
+        let sorted: Vec<f64> = {
+            let mut v = durations.to_vec();
+            v.sort();
+            v.into_iter().map(|d| d as f64).collect()
+        };
+
+        // Collect all the percentile values
+        (0..100)
+            .map(|i| {
+                let pct = {
+                    let exp = f64::from(10 * (i + 1)) / 100f64;
+                    1f64 - 0.5f64.powf(exp)
+                };
+                percentile_of_sorted(&sorted, 100f64 * pct)
+            })
+            .collect()
+    }
+
+    pub fn ct_stats(left_samples: &[u64], right_samples: &[u64]) -> CtSummary {
+        // Only construct the context (that is, percentiles and test structs) on the first run
+        let (mut tests, percentiles) = {
+            let all_samples = {
+                let mut v = left_samples.to_vec();
+                v.extend_from_slice(right_samples);
+                v
+            };
+            let pcts = prepare_percentiles(&all_samples);
+            let tests = vec![CtTest::default(); 101];
+
+            (tests, pcts)
+        };
+
+        let left_samples: Vec<f64> = left_samples.iter().map(|&n| n as f64).collect();
+        let right_samples: Vec<f64> = right_samples.iter().map(|&n| n as f64).collect();
+
+        for &left_sample in left_samples.iter() {
+            update_test_left(&mut tests[0], left_sample);
+        }
+        for &right_sample in right_samples.iter() {
+            update_test_right(&mut tests[0], right_sample);
+        }
+
+        for (test, &pct) in tests.iter_mut().skip(1).zip(percentiles.iter()) {
+            let left_cropped = left_samples.iter().filter(|&&x| x < pct);
+            let right_cropped = right_samples.iter().filter(|&&x| x < pct);
+
+            for &left_sample in left_cropped {
+                update_test_left(test, left_sample);
+            }
+            for &right_sample in right_cropped {
+                update_test_right(test, right_sample);
+            }
+        }
+
+        let (max_t, max_tau, sample_size) = {
+            // Get the test with the maximum t
+            let max_test = tests
+                .iter()
+                .max_by(|&x, &y| local_cmp(compute_t(x).abs(), compute_t(y).abs()))
+                .unwrap();
+            let sample_size = max_test.sizes.0 + max_test.sizes.1;
+            let max_t = compute_t(max_test);
+            let max_tau = max_t / (sample_size as f64).sqrt();
+
+            (max_t, max_tau, sample_size)
+        };
+
+        CtSummary {
             max_t,
             max_tau,
             sample_size,
-        } = self;
-        write!(
-            f,
-            "n == {:+0.3}M, max t = {:+0.5}, max tau = {:+0.5}, (5/tau)^2 = {}",
-            (sample_size as f64) / 1_000_000f64,
-            max_t,
-            max_tau,
-            (5f64 / max_tau).powi(2) as usize
-        )
-    }
-}
-
-#[derive(Copy, Clone, Debug, Default)]
-struct CtTest {
-    means: (f64, f64),
-    sq_diffs: (f64, f64),
-    sizes: (usize, usize),
-}
-
-// NaNs are smaller than everything
-fn local_cmp(x: f64, y: f64) -> cmp::Ordering {
-    use std::cmp::Ordering::{Equal, Greater, Less};
-    if y.is_nan() {
-        Greater
-    } else if x.is_nan() || x < y {
-        Less
-    } else if x == y {
-        Equal
-    } else {
-        Greater
-    }
-}
-
-/// Helper function: extract a value representing the `pct` percentile of a sorted sample-set,
-/// using linear interpolation. If samples are not sorted, return nonsensical value.
-fn percentile_of_sorted(sorted_samples: &[f64], pct: f64) -> f64 {
-    assert!(!sorted_samples.is_empty());
-    if sorted_samples.len() == 1 {
-        return sorted_samples[0];
-    }
-    let zero = 0f64;
-    assert!(zero <= pct);
-    let hundred = 100f64;
-    assert!(pct <= hundred);
-    let length = (sorted_samples.len() - 1) as f64;
-    let rank = (pct / hundred) * length;
-    let lrank = rank.floor();
-    let d = rank - lrank;
-    let n = lrank as usize;
-    let lo = sorted_samples[n];
-    let hi = sorted_samples[n + 1];
-    lo + (hi - lo) * d
-}
-
-/// Return the percentiles at f(1), f(2), ..., f(100) of the runtime distribution, where
-/// `f(k) = 1 - 0.5^(10k / 100)`
-pub fn prepare_percentiles(durations: &[u64]) -> Vec<f64> {
-    let sorted: Vec<f64> = {
-        let mut v = durations.to_vec();
-        v.sort();
-        v.into_iter().map(|d| d as f64).collect()
-    };
-
-    // Collect all the percentile values
-    (0..100)
-        .map(|i| {
-            let pct = {
-                let exp = f64::from(10 * (i + 1)) / 100f64;
-                1f64 - 0.5f64.powf(exp)
-            };
-            percentile_of_sorted(&sorted, 100f64 * pct)
-        })
-        .collect()
-}
-
-pub fn ct_stats(left_samples: &[u64], right_samples: &[u64]) -> CtSummary {
-    // Only construct the context (that is, percentiles and test structs) on the first run
-    let (mut tests, percentiles) = {
-        let all_samples = {
-            let mut v = left_samples.to_vec();
-            v.extend_from_slice(right_samples);
-            v
-        };
-        let pcts = prepare_percentiles(&all_samples);
-        let tests = vec![CtTest::default(); 101];
-
-        (tests, pcts)
-    };
-
-    let left_samples: Vec<f64> = left_samples.iter().map(|&n| n as f64).collect();
-    let right_samples: Vec<f64> = right_samples.iter().map(|&n| n as f64).collect();
-
-    for &left_sample in left_samples.iter() {
-        update_test_left(&mut tests[0], left_sample);
-    }
-    for &right_sample in right_samples.iter() {
-        update_test_right(&mut tests[0], right_sample);
-    }
-
-    for (test, &pct) in tests.iter_mut().skip(1).zip(percentiles.iter()) {
-        let left_cropped = left_samples.iter().filter(|&&x| x < pct);
-        let right_cropped = right_samples.iter().filter(|&&x| x < pct);
-
-        for &left_sample in left_cropped {
-            update_test_left(test, left_sample);
-        }
-        for &right_sample in right_cropped {
-            update_test_right(test, right_sample);
         }
     }
 
-    let (max_t, max_tau, sample_size) = {
-        // Get the test with the maximum t
-        let max_test = tests
-            .iter()
-            .max_by(|&x, &y| local_cmp(compute_t(x).abs(), compute_t(y).abs()))
-            .unwrap();
-        let sample_size = max_test.sizes.0 + max_test.sizes.1;
-        let max_t = compute_t(max_test);
-        let max_tau = max_t / (sample_size as f64).sqrt();
+    fn compute_t(test: &CtTest) -> f64 {
+        let &CtTest {
+            means,
+            sq_diffs,
+            sizes,
+        } = test;
+        let num = means.0 - means.1;
+        let n0 = sizes.0 as f64;
+        let n1 = sizes.1 as f64;
+        let var0 = sq_diffs.0 / (n0 - 1f64);
+        let var1 = sq_diffs.1 / (n1 - 1f64);
+        let den = (var0 / n0 + var1 / n1).sqrt();
 
-        (max_t, max_tau, sample_size)
-    };
-
-    CtSummary {
-        max_t,
-        max_tau,
-        sample_size,
+        num / den
     }
-}
 
-fn compute_t(test: &CtTest) -> f64 {
-    let &CtTest {
-        means,
-        sq_diffs,
-        sizes,
-    } = test;
-    let num = means.0 - means.1;
-    let n0 = sizes.0 as f64;
-    let n1 = sizes.1 as f64;
-    let var0 = sq_diffs.0 / (n0 - 1f64);
-    let var1 = sq_diffs.1 / (n1 - 1f64);
-    let den = (var0 / n0 + var1 / n1).sqrt();
+    fn update_test_left(test: &mut CtTest, datum: f64) {
+        test.sizes.0 += 1;
+        let diff = datum - test.means.0;
+        test.means.0 += diff / (test.sizes.0 as f64);
+        test.sq_diffs.0 += diff * (datum - test.means.0);
+    }
 
-    num / den
-}
-
-fn update_test_left(test: &mut CtTest, datum: f64) {
-    test.sizes.0 += 1;
-    let diff = datum - test.means.0;
-    test.means.0 += diff / (test.sizes.0 as f64);
-    test.sq_diffs.0 += diff * (datum - test.means.0);
-}
-
-fn update_test_right(test: &mut CtTest, datum: f64) {
-    test.sizes.1 += 1;
-    let diff = datum - test.means.1;
-    test.means.1 += diff / (test.sizes.1 as f64);
-    test.sq_diffs.1 += diff * (datum - test.means.1);
+    fn update_test_right(test: &mut CtTest, datum: f64) {
+        test.sizes.1 += 1;
+        let diff = datum - test.means.1;
+        test.means.1 += diff / (test.sizes.1 as f64);
+        test.sq_diffs.1 += diff * (datum - test.means.1);
+    }
 }
