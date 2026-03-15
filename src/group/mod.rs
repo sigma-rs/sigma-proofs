221a8d5e9db83cd72e1700f69ad06f009fb4798c
Victor Graf <victor@subgraf.dev>
2026-03-14 17:40:45 -0700

    add FromHash traits and implementations for curve25519-dalek, p256, and k256

--- src/group/mod.rs
+++ src/group/mod.rs
@@ -1,5 +1,104 @@
+use bytemuck::Zeroable;
+use digest::{Digest, ExtendableOutput, Output, XofReader};
+
 /// Implementation of multi-scalar multiplication (MSM) over scalars and points.
 pub mod msm;
 
 /// Implementation of batch serialization functions for scalars and points.
 pub mod serialization;
+
+pub trait FromUniformBytes: Sized {
+    type Bytes: AsRef<[u8]> + AsMut<[u8]> + Zeroable;
+
+    fn from_uniform_bytes(bytes: &Self::Bytes) -> Self;
+}
+
+pub trait FromHash: FromUniformBytes {
+    fn from_digest<D>(digest: D) -> Self
+    where
+        D: Digest,
+        Output<D>: AsRef<Self::Bytes>,
+    {
+        Self::from_uniform_bytes(digest.finalize().as_ref())
+    }
+
+    fn from_hash<D>(input: impl AsRef<[u8]>) -> Self
+    where
+        D: Digest,
+        Output<D>: AsRef<Self::Bytes>,
+    {
+        Self::from_digest(D::new().chain_update(input))
+    }
+
+    fn from_xof<X: XofReader>(xof: &mut X) -> Self {
+        let mut bytes = Self::Bytes::zeroed();
+        xof.read(bytes.as_mut());
+        Self::from_uniform_bytes(&bytes)
+    }
+
+    fn from_hash_xof<D>(input: impl AsRef<[u8]>) -> Self
+    where
+        D: ExtendableOutput + Default,
+    {
+        let mut bytes = Self::Bytes::zeroed();
+        D::digest_xof(input, bytes.as_mut());
+        Self::from_uniform_bytes(&bytes)
+    }
+}
+
+#[cfg(feature = "curve25519-dalek")]
+mod curve25519 {
+    use curve25519_dalek::RistrettoPoint;
+
+    use super::{FromHash, FromUniformBytes};
+
+    impl FromUniformBytes for RistrettoPoint {
+        type Bytes = [u8; 64];
+
+        fn from_uniform_bytes(bytes: &Self::Bytes) -> Self {
+            Self::from_uniform_bytes(bytes)
+        }
+    }
+
+    impl FromHash for RistrettoPoint {}
+}
+
+#[cfg(feature = "k256")]
+mod k256 {
+    use elliptic_curve::hash2curve::{FromOkm, MapToCurve};
+    use k256::{FieldElement, ProjectivePoint};
+
+    use super::FromUniformBytes;
+
+    impl FromUniformBytes for ProjectivePoint {
+        type Bytes = [u8; 96];
+
+        fn from_uniform_bytes(bytes: &Self::Bytes) -> Self {
+            fn to_curve_nonuniform(bytes: &[u8; 48]) -> ProjectivePoint {
+                FieldElement::from_okm(bytes.into()).map_to_curve()
+            }
+            let [b0, b1] = bytemuck::cast_ref(bytes);
+            to_curve_nonuniform(b0) + to_curve_nonuniform(b1)
+        }
+    }
+}
+
+#[cfg(feature = "p256")]
+mod p256 {
+    use elliptic_curve::hash2curve::{FromOkm, MapToCurve};
+    use p256::{FieldElement, ProjectivePoint};
+
+    use super::FromUniformBytes;
+
+    impl FromUniformBytes for ProjectivePoint {
+        type Bytes = [u8; 96];
+
+        fn from_uniform_bytes(bytes: &Self::Bytes) -> Self {
+            fn to_curve_nonuniform(bytes: &[u8; 48]) -> ProjectivePoint {
+                FieldElement::from_okm(bytes.into()).map_to_curve()
+            }
+            let [b0, b1] = bytemuck::cast_ref(bytes);
+            to_curve_nonuniform(b0) + to_curve_nonuniform(b1)
+        }
+    }
+}
