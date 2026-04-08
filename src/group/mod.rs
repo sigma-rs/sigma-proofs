11e84d6997a2b437319eae28f0ac4332d726b26f
Victor Graf <victor@subgraf.dev>
2026-04-08 11:06:20 -0700

    fix and integrate the expand_message_xmd into FromDigest

--- src/group/mod.rs
+++ src/group/mod.rs
@@ -1,10 +1,12 @@
 use bytemuck::Zeroable;
 use digest::{
-    array::{Array, ArraySize},
+    array::{Array, ArraySize, AssocArraySize},
     common::BlockSizeUser,
     typenum::Unsigned,
     Digest, ExtendableOutput, Output, XofReader,
 };
+
+// TODO: Once the code is looking good, move these from digest functions into their own module.
 
 /// Implementation of multi-scalar multiplication (MSM) over scalars and points.
 pub mod msm;
@@ -15,19 +17,32 @@ pub trait FromUniformBytes: Sized {
     fn from_uniform_bytes(bytes: &Self::Bytes) -> Self;
 }
 
+/// Create a block of zeroes to use as padding as per RFC9380.
+///
+/// ```rust
+/// use sha2::Sha256;
+/// use sigma_proofs::group::zero_pad;
+///
+/// let zeroes = zero_pad::<Sha256>();
+/// assert!(zeroes.iter().all(|b| *b == 0));
+/// ```
+pub fn zero_pad<D: BlockSizeUser>() -> Array<u8, D::BlockSize> {
+    Array::default()
+}
+
 /// Generates a uniformly random byte array of length `N` from a domain separator and message.
 ///
 /// This is an implementation of expand_message_xmd from RFC9380.
 ///
 /// <!-- TODO: Add panic conditions -->
-fn expand_message_xmd<D: Digest + BlockSizeUser, N: ArraySize>(
+pub fn expand_message_xmd<D: Digest + BlockSizeUser, N: ArraySize>(
     domain_separator: &[u8],
     message: &[u8],
 ) -> Array<u8, N> {
     // Compress the message and domain separator into the digest state.
     let message_digest = D::new()
         // Prefix with a block of zeroes and the block length, as discussed in RFC9380 Section 10.6
-        .chain_update(Array::<u8, D::BlockSize>::default())
+        .chain_update(zero_pad::<D>())
         // Add in the message.
         // NOTE: The length is not included here.
         .chain_update(message);
@@ -41,7 +56,7 @@ fn expand_message_xmd<D: Digest + BlockSizeUser, N: ArraySize>(
 /// expand_message_xmd from RFC9380.
 ///
 /// <!-- TODO: Add panic conditions -->
-fn expand_message_digest_xmd<D: Digest, N: ArraySize>(
+pub fn expand_message_digest_xmd<D: Digest, N: ArraySize>(
     domain_separator: &[u8],
     message_digest: D,
 ) -> Array<u8, N> {
@@ -56,7 +71,7 @@ fn expand_message_digest_xmd<D: Digest, N: ArraySize>(
         "expand_message_xmd requires the output length to be at most 65535 bytes"
     );
     assert!(
-        N::USIZE / <D::OutputSize as Unsigned>::USIZE <= u8::MAX as usize,
+        N::USIZE.div_ceil(<D::OutputSize as Unsigned>::USIZE) <= u8::MAX as usize,
         "expand_message_xmd requires the output length to be at most 255 times the digest length"
     );
 
@@ -70,24 +85,17 @@ fn expand_message_digest_xmd<D: Digest, N: ArraySize>(
         .chain_update(u8::try_from(domain_separator.len()).unwrap().to_be_bytes())
         .finalize();
 
-    // Expand the message to fill the output array.
+    // Expand the message to fill the output array with b_1 || ... || b_ell.
     let mut output = Array::<u8, N>::default();
-    let mut output_chunks = output
-        .chunks_mut(<D::OutputSize as Unsigned>::USIZE)
-        .enumerate();
+    let output_chunks = output.chunks_mut(<D::OutputSize as Unsigned>::USIZE);
 
-    // Write the message digest to the first chunk.
-    output_chunks.next().map(|(_, chunk_0)| {
-        chunk_0.copy_from_slice(&digest_0[..chunk_0.len()]);
-    });
-
-    // Using a counter and chaining to previous chunks, fill the rest of the buffer.
+    // Using a counter and chaining to previous digests, fill the output buffer.
     let mut prev_digest: Option<Output<D>> = None;
-    for (i, output_chunk_i) in output_chunks {
+    for (i, output_chunk_i) in output_chunks.enumerate() {
         // XOR the message digest with the previous digest, except on the first iteration.
         // NOTE: RFC9380 includes this to bolster defense against nonideal hash behavior.
         let mut mixed_digest = digest_0.clone();
-        if let Some(prev_digest) = prev_digest {
+        if let Some(prev_digest) = &prev_digest {
             // NOTE: This assert can be checked at compile time.
             // TODO: Is there any way to turn this into a compile-time assert, given that the size
             // of the arrays is determined by a generic parameter.
@@ -97,34 +105,39 @@ fn expand_message_digest_xmd<D: Digest, N: ArraySize>(
             }
         }
 
-        let chunk_i = D::new()
+        let b_i = D::new()
             // Add the fixed-length message digest.
             .chain_update(&mixed_digest)
             // Add the index, starting from one.
-            .chain_update(u8::try_from(i).unwrap().to_be_bytes())
-            // Ass the domain separator and length.
+            .chain_update(u8::try_from(i + 1).unwrap().to_be_bytes())
+            // Add the domain separator and length.
             .chain_update(domain_separator)
             .chain_update(u8::try_from(domain_separator.len()).unwrap().to_be_bytes())
             .finalize();
 
         // Copy the digest into the output chunk.
         // NOTE: This will copy the entire digest except on the last iteration.
-        output_chunk_i.copy_from_slice(&chunk_i[..output_chunk_i.len()]);
+        output_chunk_i.copy_from_slice(&b_i[..output_chunk_i.len()]);
 
-        prev_digest = Some(chunk_i);
+        prev_digest = Some(b_i);
     }
 
     output
 }
 
-pub trait FromDigest<D: Digest + BlockSizeUser>: FromUniformBytes {
+pub trait FromDigest<D>: FromUniformBytes
+where
+    D: Digest + BlockSizeUser,
+    Self::Bytes: AssocArraySize + From<Array<u8, <Self::Bytes as AssocArraySize>::Size>>,
+{
     // TODO: Provide an example of using this with XofFixedWrapper
     fn from_digest(digest: D) -> Self {
-        Self::from_uniform_bytes(digest.finalize().as_ref())
+        let uniform_bytes = expand_message_digest_xmd(b"".as_slice(), digest).into();
+        Self::from_uniform_bytes(&uniform_bytes)
     }
 
     fn from_hash(input: impl AsRef<[u8]>) -> Self {
-        Self::from_digest(D::new().chain_update(input))
+        Self::from_digest(D::new().chain_update(zero_pad::<D>()).chain_update(input))
     }
 }
 
@@ -148,6 +161,11 @@ pub trait FromXof: FromUniformBytes {
 #[cfg(feature = "curve25519-dalek")]
 mod curve25519 {
     use curve25519_dalek::RistrettoPoint;
+    use digest::{
+        array::{Array, AssocArraySize},
+        common::BlockSizeUser,
+        Digest,
+    };
 
     use super::{FromDigest, FromUniformBytes, FromXof};
 
@@ -159,12 +177,22 @@ mod curve25519 {
         }
     }
 
-    impl FromDigest for RistrettoPoint {}
+    impl<D> FromDigest<D> for RistrettoPoint
+    where
+        D: Digest + BlockSizeUser,
+        Self::Bytes: AssocArraySize + From<Array<u8, <Self::Bytes as AssocArraySize>::Size>>,
+    {
+    }
     impl FromXof for RistrettoPoint {}
 }
 
 #[cfg(feature = "k256")]
 mod k256 {
+    use digest::{
+        array::{Array, AssocArraySize},
+        common::BlockSizeUser,
+        Digest,
+    };
     use elliptic_curve::hash2curve::{FromOkm, MapToCurve};
     use k256::{FieldElement, ProjectivePoint};
 
@@ -182,12 +210,22 @@ mod k256 {
         }
     }
 
-    impl FromDigest for ProjectivePoint {}
+    impl<D> FromDigest<D> for ProjectivePoint
+    where
+        D: Digest + BlockSizeUser,
+        Self::Bytes: AssocArraySize + From<Array<u8, <Self::Bytes as AssocArraySize>::Size>>,
+    {
+    }
     impl FromXof for ProjectivePoint {}
 }
 
 #[cfg(feature = "p256")]
 mod p256 {
+    use digest::{
+        array::{Array, AssocArraySize},
+        common::BlockSizeUser,
+        Digest,
+    };
     use elliptic_curve::hash2curve::{FromOkm, MapToCurve};
     use p256::{FieldElement, ProjectivePoint};
 
@@ -205,7 +243,12 @@ mod p256 {
         }
     }
 
-    impl FromDigest for ProjectivePoint {}
+    impl<D> FromDigest<D> for ProjectivePoint
+    where
+        D: Digest + BlockSizeUser,
+        Self::Bytes: AssocArraySize + From<Array<u8, <Self::Bytes as AssocArraySize>::Size>>,
+    {
+    }
     impl FromXof for ProjectivePoint {}
 }
 
@@ -224,7 +267,7 @@ mod tests {
         use sha2::{Digest as _, Sha256, Sha512};
 
         // NOTE: RistrettoPoint has directly implemented methods called from_hash and from_digest.
-        let _: RistrettoPoint = FromDigest::from_hash::<Sha512>(b"hello");
+        let _: RistrettoPoint = FromDigest::<Sha512>::from_hash(b"hello");
         let _: RistrettoPoint = FromDigest::from_digest(Sha512::new().chain_update(b"hello"));
     }
 
@@ -238,11 +281,13 @@ mod tests {
         let _ = RistrettoPoint::from_xof(&mut Shake128::default().chain(b"hello").finalize_xof());
 
         // NOTE: RistrettoPoint has directly implemented methods called from_hash and from_digest.
-        let _: RistrettoPoint = FromDigest::from_hash::<XofFixedWrapper<Shake128, U64>>(b"hello");
+        let _: RistrettoPoint = FromDigest::<XofFixedWrapper<Shake128, U64>>::from_hash(b"hello");
         let _: RistrettoPoint =
             FromDigest::from_digest(XofFixedWrapper::<Shake128, U64>::new().chain_update(b"hello"));
 
-        let _ = p256::ProjectivePoint::from_hash::<XofFixedWrapper<Shake128, U96>>(b"hello");
+        // TODO: This syntax suffers from an awkwardness with having the generic param on the
+        // trait. Figure out the best way to address this.
+        //let _ = p256::ProjectivePoint::from_hash(b"hello");
         let _ = p256::ProjectivePoint::from_digest(
             XofFixedWrapper::<Shake128, U96>::new().chain_update(b"hello"),
         );
