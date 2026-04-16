3f46fedff388388295eb1e2159892bd02e64bd7d
Armando Faz <armfazh@cloudflare.com>
2026-04-16 11:12:57 -0700

    tests: Implement TestDrng using spongefish.

--- tests/spec/rng.rs
+++ tests/spec/rng.rs
@@ -2,38 +2,32 @@ use rand_core::{
     impls::{next_u32_via_fill, next_u64_via_fill},
     CryptoRng, Error, RngCore,
 };
-use sha3::digest::{ExtendableOutput, Update, XofReader};
 
-pub struct TestDrng {
-    state: sha3::Shake128,
-    squeeze_offset: usize,
-}
+use spongefish::{instantiations::Shake128, DuplexSpongeInterface as _};
+
+/// TestDrng from draft-sigma specification [1].
+///
+/// [1] https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-sigma-protocols-02#appendix-A.1
+pub struct TestDrng(Shake128);
 
 impl TestDrng {
     pub fn from_seed(seed_label: &[u8]) -> Self {
+        const DOMAIN: &[u8] = b"sigma-proofs/TestDRNG/SHAKE128";
         let mut initial_block = [0u8; 168];
-        let domain = b"sigma-proofs/TestDRNG/SHAKE128";
-        initial_block[..domain.len()].copy_from_slice(domain);
+        initial_block[..DOMAIN.len()].copy_from_slice(DOMAIN);
 
-        let mut state = sha3::Shake128::default();
-        state.update(&initial_block);
-        state.update(&fixed_seed(seed_label));
-        Self {
-            state,
-            squeeze_offset: 0,
-        }
-    }
-
-    fn squeeze_into(&mut self, buf: &mut [u8]) {
-        let end = self.squeeze_offset + buf.len();
-        let mut full = vec![0u8; end];
-        self.state.clone().finalize_xof().read(&mut full);
-        buf.copy_from_slice(&full[self.squeeze_offset..end]);
-        self.squeeze_offset = end;
+        let mut sponge = Shake128::default();
+        sponge.absorb(&initial_block);
+        sponge.absorb(&fixed_seed(seed_label));
+        Self(sponge)
     }
 }
 
 fn fixed_seed(label: &[u8]) -> [u8; 32] {
+    if label.len() > 32 {
+        panic!("seed label length must be less or equal to 32 bytes")
+    }
+
     let mut seed = [0u8; 32];
     seed[..label.len()].copy_from_slice(label);
     seed
@@ -51,7 +45,7 @@ impl RngCore for TestDrng {
     }
 
     fn fill_bytes(&mut self, dst: &mut [u8]) {
-        self.squeeze_into(dst)
+        self.0.squeeze(dst);
     }
 
     fn try_fill_bytes(&mut self, dst: &mut [u8]) -> Result<(), Error> {
